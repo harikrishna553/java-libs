a60dd4ab30c90a6d17da6bfcdf5252d6641ddc7b
harikrishna553 <harikrishna553@gmail.com>
2022-02-20 21:05:32 +0530

    Configure logback with a configuration file

---
+++ logback/logback-configuration-hello-world/src/main/java/com/sample/app/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.sample.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+		logger.trace("Trace message");
+		logger.debug("Debug message");
+		logger.info("Info message");
+		logger.warn("Warning message");
+		logger.error("Error message");
+
+	}
+}
