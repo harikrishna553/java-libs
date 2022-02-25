1c17542b90cd167171853d48c445477691983683
harikrishna553 <harikrishna553@gmail.com>
2022-02-25 11:28:30 +0530

    Read environment variables from logback configuration file

---
+++ logback/read-env-variables-in-logback-config/src/main/java/com/sample/app/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.sample.app;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.core.joran.spi.JoranException;
+
+public class HelloWorld {
+	private static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+	public static void main(String[] args) throws JoranException, IOException {
+
+		logger.trace("Trace message");
+		logger.debug("Debug message");
+		logger.info("Info message");
+		logger.warn("Warning message");
+		logger.error("Error message");
+
+	}
+}
