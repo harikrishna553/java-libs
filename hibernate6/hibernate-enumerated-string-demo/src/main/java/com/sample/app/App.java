f74911eab7bde7f14b4879c11722a1425fa21409
harikrishna553 <harikrishna553@gmail.com>
2022-08-12 12:16:24 +0530

    Hibernate @Enumerated with String attribute demo

---
+++ hibernate6/hibernate-enumerated-string-demo/src/main/java/com/sample/app/App.java
@@ -0,0 +1,48 @@
+package com.sample.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.sample.app.entity.Person;
+import com.sample.app.enums.Gender;
+
+public class App {
+	private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+
+			final StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+					.configure("hibernate.cfg.xml").build();
+
+			final Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+
+			return metaData.getSessionFactoryBuilder().build();
+
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
+		}
+
+	}
+
+	public static void main(final String args[]) {
+
+		Person p1 = new Person(1, "Srinu", Gender.MALE);
+		Person p2 = new Person(2, "Swetha", Gender.FEMALE);
+
+		try (final Session session = SESSION_FACTORY.openSession()) {
+			session.beginTransaction();
+
+			session.persist(p1);
+			session.persist(p2);
+
+			session.getTransaction().commit();
+
+		}
+
+	}
+}
