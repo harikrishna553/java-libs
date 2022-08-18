89f6b060831ec299b829f94587d1e7f2bd0dbc08
harikrishna553 <harikrishna553@gmail.com>
2022-08-18 16:07:24 +0530

    Map uuid

---
+++ hibernate6/hibernate-uuid-mapping/src/main/java/com/sample/app/App.java
@@ -0,0 +1,66 @@
+package com.sample.app;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.sample.app.entity.Employee;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+
+public class App {
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	private static <T> List<T> loadAllData(Class<T> clazz, Session session) {
+		final CriteriaBuilder builder = session.getCriteriaBuilder();
+		final CriteriaQuery<T> criteria = builder.createQuery(clazz);
+		criteria.from(clazz);
+		return session.createQuery(criteria).getResultList();
+	}
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			if (sessionFactory == null) {
+				StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+						.configure("hibernate.cfg.xml").build();
+
+				Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+
+				sessionFactory = metaData.getSessionFactoryBuilder().build();
+			}
+			return sessionFactory;
+		} catch (Throwable ex) {
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static void main(String args[]) throws ClassNotFoundException, IOException, URISyntaxException {
+
+		Employee emp = new Employee(UUID.randomUUID(), "Krishna", Locale.ENGLISH);
+
+		try (Session session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			session.persist(emp);
+			session.flush();
+			session.getTransaction().commit();
+
+			List<Employee> emps = loadAllData(Employee.class, session);
+			for (Employee emp1 : emps) {
+				System.out.println(emp1);
+
+			}
+
+		}
+
+	}
+}
