bcb7bd3d51f07944f0716511d7c5067e93fa3772
harikrishna553 <harikrishna553@gmail.com>
2022-08-22 16:39:27 +0530

    Generate identifiers using AUTO strategy

---
+++ hibernate6/hibernate-auto-generate-identifier-demo/src/main/java/com/sample/app/entity/Employee.java
@@ -0,0 +1,43 @@
+package com.sample.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	private String name;
+
+	public Employee(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+
+}
