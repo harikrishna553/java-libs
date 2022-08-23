5f70ad776c9737a4368675ee7efdb18a6a33fccb
harikrishna553 <harikrishna553@gmail.com>
2022-08-23 16:33:45 +0530

    Generate identifiers using TABLE strategy

---
+++ hibernate6/hibernate-table-strategy-demo/src/main/java/com/sample/app/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.sample.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(
+		strategy = GenerationType.TABLE,
+		generator = "my-table-generator"
+	)
+	@TableGenerator(
+		name =  "my-table-generator",
+		table = "my_table_identifier",
+		pkColumnName = "table_name",
+		valueColumnName = "record_id",
+		allocationSize = 5
+	)
+	private Integer id;
+
+	private String name;
+
+	public Employee(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+
+}
