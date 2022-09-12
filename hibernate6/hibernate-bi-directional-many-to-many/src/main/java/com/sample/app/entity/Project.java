906d8a869945988cdc2f11e78cfae84d63ee12d1
harikrishna553 <harikrishna553@gmail.com>
2022-09-12 13:25:57 +0530

    Hibernate Many to Many association example

---
+++ hibernate6/hibernate-bi-directional-many-to-many/src/main/java/com/sample/app/entity/Project.java
@@ -0,0 +1,77 @@
+package com.sample.app.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "project")
+public class Project {
+	@Id
+	private int projectId;
+
+	private String projectName;
+
+	public Project() {
+	}
+
+	public Project(int projectId, String projectName) {
+		this.projectId = projectId;
+		this.projectName = projectName;
+	}
+
+	@ManyToMany(mappedBy = "projects")
+	private List<Employee> employees = new ArrayList<>();
+
+	public int getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(projectId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Project other = (Project) obj;
+		return projectId == other.projectId;
+	}
+
+	@Override
+	public String toString() {
+		return "Project [projectId=" + projectId + ", projectName=" + projectName + "]";
+	}
+
+}
