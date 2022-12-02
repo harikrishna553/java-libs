d3e46a534f05bcc93de3feebde69cac01e02f55c
harikrishna553 <harikrishna553@gmail.com>
2022-12-02 21:14:41 +0530

    Inject HttpRequest using ServerRequestContext

---
+++ micronaut/rest/micronaut-inject-http-request-using-server-request-context/src/main/java/com/sample/app/controller/HelloController.java
@@ -0,0 +1,34 @@
+package com.sample.app.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.micronaut.http.HttpParameters;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.context.ServerRequestContext;
+
+@Controller("/hello")
+public class HelloController {
+
+	@Get(value = "/demo1", produces = MediaType.APPLICATION_JSON)
+	public HttpResponse<Map<String, String>> demo1() {
+
+		final HttpRequest<?> httpRequest = ServerRequestContext.currentRequest() //
+				.orElseThrow(() -> new RuntimeException("No request present"));
+
+		final HttpParameters httpParameters = httpRequest.getParameters();
+		final String city = httpParameters.get("city");
+		final String country = httpParameters.get("country");
+
+		final Map<String, String> data = new HashMap<>();
+		data.put("city", city);
+		data.put("country", country);
+
+		return HttpResponse.ok(data);
+	}
+
+}
