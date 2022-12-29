fbb415056038db3616b0e6cef0a12f35a45b1242
harikrishna553 <harikrishna553@gmail.com>
2022-12-30 03:16:58 +0530

    Add more JinJava examples

---
+++ template-engines/jinjava-demo/src/main/java/com/sample/app/tags/UnlessTagDemo.java
@@ -0,0 +1,18 @@
+package com.sample.app.tags;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import com.hubspot.jinjava.Jinjava;
+import com.sample.app.util.FileUtil;
+
+public class UnlessTagDemo {
+	public static void main(String[] args) throws IOException {
+		final Jinjava jinjava = new Jinjava();
+
+		final String template = FileUtil.resourceAsString("tags/unless.jinja");
+
+		final String finalDocument = jinjava.render(template, Collections.emptyMap());
+		System.out.println(finalDocument);
+	}
+}
