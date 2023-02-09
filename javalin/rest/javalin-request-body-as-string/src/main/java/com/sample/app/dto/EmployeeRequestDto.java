05edaede3004ca06ffe4501ab4a2ac70c0010e9a
harikrishna553 <harikrishna553@gmail.com>
2023-02-09 16:29:29 +0530

    read requst body as string

---
+++ javalin/rest/javalin-request-body-as-string/src/main/java/com/sample/app/dto/EmployeeRequestDto.java
@@ -0,0 +1,24 @@
+package com.sample.app.dto;
+
+public class EmployeeRequestDto {
+
+	private String name;
+	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
