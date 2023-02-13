dcdbfaf40d1f9129ba933acb5152517377acbe52
harikrishna553 <harikrishna553@gmail.com>
2023-02-13 13:06:39 +0530

    Validate form parameter

---
+++ javalin/rest/javalin-validate-form-parameter/src/main/java/com/sample/app/App.java
@@ -0,0 +1,39 @@
+package com.sample.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sample.app.util.JsonUtil;
+
+import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Javalin javalin = Javalin.create();
+
+		javalin.get("/", (ctx) -> {
+			ctx.result("Welcome to Javalin programming!!!!");
+		});
+
+		javalin.post("/form-params-demo", (ctx) -> {
+
+			String name = ctx.formParamAsClass("name", String.class)
+					.getOrDefault("no_name");
+			
+			String age = ctx.formParamAsClass("age", String.class)
+					.getOrDefault("no_age");
+
+			Map<String, Object> receivedData = new HashMap<>();
+			receivedData.put("name", name);
+			receivedData.put("age", age);
+
+			ctx.result(JsonUtil.marshal(receivedData)).status(HttpStatus.CREATED);
+		});
+
+		javalin.start(8080);
+	}
+
+}
