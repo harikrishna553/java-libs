43a46fc0d6026066cd9ccb5d006752d13636e0c7
harikrishna553 <harikrishna553@gmail.com>
2023-02-14 14:49:45 +0530

    Read request cookies as a map

---
+++ javalin/rest/javalin-read-request-cookies-as-map/src/main/java/com/sample/app/util/JsonUtil.java
@@ -0,0 +1,13 @@
+package com.sample.app.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+
+	public static String getJson(Object obj) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(obj);
+	}
+
+}
