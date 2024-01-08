936f24b0e91e2904a878b6efb6929f4fd7fcbeb2
harikrishna553 <harikrishna553@gmail.com>
2024-01-08 21:37:45 +0530

    Sharding demo example

---
+++ system-design/sharding-demo/src/main/java/com/sample/app/TestFileContent.java
@@ -0,0 +1,11 @@
+package com.sample.app;
+
+public class TestFileContent {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10000; i++) {
+			System.out.println("Line " + i);
+		}
+	}
+
+}
