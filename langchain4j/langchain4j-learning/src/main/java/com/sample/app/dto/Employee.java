e8b55f65783dec83fc407d77efb3a0c638684ff5
Harikrishna <harikrishna.gurram@walmart.com>
2025-06-13 20:11:07 +0530

    Updated dto langchain annotations

--- langchain4j/langchain4j-learning/src/main/java/com/sample/app/dto/Employee.java
+++ langchain4j/langchain4j-learning/src/main/java/com/sample/app/dto/Employee.java
@@ -1,3 +1,10 @@
 package com.sample.app.dto;
 
-public record Employee(String name, int age, double height, boolean married) {}
+import dev.langchain4j.model.output.structured.Description;
+
+@Description("An Employee")
+public record Employee(@Description("Employee's first and last name, for example: Krishna") String name,
+		@Description("Employee's age, for example: 42") int age,
+		@Description("Employee's height in meters, for example: 1.78") double height,
+		@Description("is Employee married or not, for example: false") boolean married) {
+}
